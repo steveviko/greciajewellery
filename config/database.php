b18c9bdb19fc6b99b76befcb62e5f5812932253e
Steven Opio <stevenviko@gmail.com>
2021-01-24 23:03:21 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
 class Database{
  
     // specify database credentials/properties
-    private $host = "localhost";
-    private $db_name = "ecomm";
-    private $username = "root";
-    private $password = "";
+    private $host = "remotemysql.com";
+    private $db_name = "vvUhx6iGfj";
+    private $username = "vvUhx6iGfj";
+    private $password = "f4VccaNSzz";
     public $conn;
  
     // get the database connection
